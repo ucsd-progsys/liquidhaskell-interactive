193f3600ba0181c8b3073d1cccb9bd02f9ceba88
Ranjit Jhala <jhala@cs.ucsd.edu>
2015-11-13 10:21:13 -0800

    cleanup using daemons library

--- src/Main.hs
+++ src/Main.hs
@@ -1,16 +1,61 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
 
-import Types
-import Network (PortID(UnixSocket), connectTo)
+module Main where
+
+import Control.Concurrent.MVar ( MVar, newMVar, modifyMVar )
+import Data.ByteString.Char8 ( ByteString )
+import Data.Default ( def )
+import Data.Serialize ( Serialize )
+import Data.String ( fromString )
+import qualified Data.HashMap.Strict as M
+import GHC.Generics
+import System.Environment ( getArgs )
+import System.Daemon
+
+data Command = Put ByteString ByteString
+             | Get ByteString
+               deriving ( Generic, Show )
+
+instance Serialize Command
+
+data Response = Failed String
+              | Value ByteString
+                deriving ( Generic, Show )
+
+instance Serialize Response
+
+type Book = M.HashMap ByteString ByteString
+
+handleCommand :: MVar Book -> Command -> IO Response
+handleCommand bookVar comm = modifyMVar bookVar $ \book -> return $
+    case comm of
+      Get key -> ( book
+                 , maybe (Failed "not found") Value (M.lookup key book) )
+      Put key value -> ( M.insert key value book
+                       , Value "ok" )
+
+
 
 main :: IO ()
 main = do
-  cmd <- getCommand
-  putStrLn $ "Whats that? " ++ show cmd
+    bookVar <- newMVar M.empty
+    let options = def { daemonPort = 7856 }
+    ensureDaemonRunning "memo" options (handleCommand bookVar)
+    args <- getArgs
+    let args' = map fromString args
+    res <- case args' of
+      ["get", key]        -> runClient "localhost"  7856 (Get key)
+      ["put", key, value] -> runClient "localhost"  7856 (Put key value)
+      _                   -> error "invalid command"
+    print (res :: Maybe Response)
 
-
-socketPath :: FilePath
-socketPath = "/Users/rjhala/tmp/.lhi.sock"
-
-
--- This is how the *client* runs
--- serverCommand :: FilePath -> Command -> CommandExtra -> IO ()
+{-
+% dist/build/memo/memo get apples
+Daemon started on port 7856
+Just (Failed "not found")
+% dist/build/memo/memo put apples 23
+Just (Value "ok")
+% dist/build/memo/memo get apples
+Just (Value "23")
+-}
